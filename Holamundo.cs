0c60269a63b29ceb0787e5c360a68110aa88c228
Caleb <chriscaleb@gmail.com>
2025-07-15 10:28:28 -0500

    agregar 15

---
+++ Holamundo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola, mundo!");
+            console.WriteLine("Bienvenido al mundo de C#!");
+        }
+    }
+}
+// This is a simple C# program that prints "¡Hola, mundo!" to the console.
+
---
+++ variables.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 42;
+            double pi = 3.14;
+            float temperature = 36.6f;
+            decimal price = 19.99m;
+            char letter = 'A';
+            string greeting = "Hello, World!";
+            bool isActive = true;
+        }
+    }
+}
